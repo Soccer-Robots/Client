ed0639374d492958f6448d2654e0918d64a8a290
daguilar2004 <118065951+daguilar2004@users.noreply.github.com>
2026-08-30 12:55:43 -0500

    test

---
+++ components/NavBar/Profile.vue
@@ -0,0 +1,44 @@
+<!-- template to show the username, profile picture, and logout button -->
+<template>
+    <div class="flex md:flex md:flex-grow flex-row-reverse space-x-2 space-x-reverse mt-5 mr-2 mb-2">
+        <div>
+            <div class="">
+            <!--We have  a button where on click, we'll call the logout function.-->
+                <button
+                    class="hover:text-gray-300 transition font-semibold duration-400 text-white font-bold py-2 px-2 rounded-lg"
+                    style="background-color:#FF0000" @click="logout"> {{ "Log Out" }}</button>
+            </div>
+        </div>
+        <div class="profileTemp">
+            <img src="../public/Profile.svg">
+        </div>
+        <div class="mt-1">
+            <p>{{ username }}</p>
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import { navigateTo } from '#app'
+
+    import type { Player } from '@prisma/client'
+    //to get play info like username, we use the cookie we have of them and find their username from it
+    const getUser = useCookie<Partial<Player>>('sruser')
+    let username = getUser.value?.username as string
+    //clear cookis and send user to homepage
+    const logout = async () => { 
+        console.log("Logging out user from Profile.vue");
+         try {
+
+            await $fetch('/api/user-logout', { method: 'POST' })
+            await navigateTo('/')
+
+        } catch (error) {
+            console.error("Logout failed:", error);
+        }
+    }
+</script>
+
+<style setup lang="ts">
+</style>
